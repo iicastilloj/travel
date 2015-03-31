926036bd4fbc1eed709a92beda8db04b6b2adfc9
iicastilloj <iicastillojt@gmail.com>
2015-03-30 23:22:07 -0600

    Meta

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Tu encuentro vacacional</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Custom styles for this template -->
+		<link href="css/travels.css" rel="stylesheet">
+
+		<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+		<!--[if lt IE 9]><script src="..assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+		<script src="js/ie-emulation-modes-warning.js"></script>
+		<!-- Modernizr -->
+		<script src="js/modernizr.js"></script>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+		<!--header-->
+		<header>
+			<div class="container">
+				<div class="col-md-2"><i class="icon-logo"></i></div>
+				<div class="col-md-8"></div>
+				<div class="col-md-2">
+					<div class="gosite">
+						<a href="#">Ir al sitio</a>
+					</div>
+				</div>
+			</div> 
+		</header>
+		<!--/header-->
+		<!--section-->
+		<section>
+			<div class="container">
+				<div class="col-md-2">&nbsp;</div>
+					<div class="col-md-8">
+						<p class="travels-cpvpen">Cuando pienses en vacacionar, piensa en nosotros</p>
+						<p class="travels-tcv">tu encuentro vacacional</p>          
+					</div>
+					<div class="col-md-2">&nbsp;</div>        
+			</div>
+		</section>
+		<!--/section-->
+			<!--.main-content-->
+			<div class="main-content">
+				<div class="container">
+					<div class="col-md-2">&nbsp;</div>
+					<div class="col-md-8">
+						<p><span class="blue">Recomiéndanos</span> al menos a cuatro amigos para que disfruten de <br /> 
+							las bondades de <span class="bold">IL ENCUENTRO</span> y recibe el <span class="bluebold">Código Promocional:</span></p>
+						<!--form-->
+						<form action="" class="travel-form">
+							<!-- .col-md-12 -->
+							<div class="col-md-12">
+								<div class="col-md-5">
+									<input class="col-md-12" type="text" placeholder="Nombre 1">
+								</div>
+								<div class="col-md-7">
+									<input class="col-md-12" type="email" placeholder="Correo 1">
+								</div>
+							</div>
+							<!--/.col-md-12 -->
+							<!-- .col-md-12 -->
+							<div class="col-md-12">
+								<div class="col-md-5">
+									<input class="col-md-12" type="text" placeholder="Nombre 2">
+								</div>
+								<div class="col-md-7">
+									<input class="col-md-12" type="email" placeholder="Correo 2">
+								</div>
+							</div>
+							<!--/.col-md-12 -->
+							<!-- .col-md-12 -->
+							<div class="col-md-12">
+								<div class="col-md-5">
+									<input class="col-md-12" type="text" placeholder="Nombre 3">
+								</div>
+								<div class="col-md-7">
+									<input class="col-md-12" type="email" placeholder="Correo 3">
+								</div>
+							</div>
+							<!--/.col-md-12 -->
+							<!-- .col-md-12 -->
+							<div class="col-md-12">
+								<div class="col-md-5">
+									<input class="col-md-12" type="text" placeholder="Nombre 4">
+								</div>
+								<div class="col-md-7">
+									<input class="col-md-12" type="email" placeholder="Correo 4">
+								</div>
+							</div>
+							<!--/.col-md-12 -->
+							<!-- .col-md-12 -->
+							<div class="col-md-12">
+								<div class="col-md-5">
+								</div>
+								<div class="col-md-7">
+									<input class="col-md-4" type="submit" value="Enviar">
+								</div>
+							</div>
+							<!--/.col-md-12 -->
+						</form>
+						 <!--/form-->
+					</div>
+					<div class="col-md-2">&nbsp;</div>
+				</div>
+			</div>
+			<!--/.main-content-->
+			<!--.facilities-content-->
+			<div class="facilities-content">
+				<div class="container">
+					<p>Conoce nuestras instalaciones</p>
+					<div class="row">
+						<div class="col-md-4"><a href="#">San Antonio</a></div>
+						<div class="col-md-4"><a href="#">Acapulco</a></div>
+						<div class="col-md-4"><a href="#">Cancún</a></div>
+					</div>
+					
+						<!--.flexslider-->
+						  <div class="flexslider">
+						  	<!---->
+							<ul class="slides">
+							  <li>
+								<img src="images/0.png" />
+							  </li>
+							  <li>
+								<img src="images/0.png" />
+							  </li>
+							  <li>
+								<img src="images/0.png" />
+							  </li>
+							  <li>
+								<img src="images/0.png" />
+							  </li>
+							</ul>
+						  </div>
+						<!--/.flexslider-->
+				</div>
+			</div>
+			<!--.facilities-content-->
+			<!--.projectdetails-content-->
+			<div class="projectdetails-content">
+				<div class="container">
+					<p class="projectabout">Conoce más acerca  del proyecto</p>
+					<div class="row">
+						<div class="col-md-3">
+							<ul>
+								<li><a href="#">¿Quiénes somos?</a></li>
+								<li><a href="#">¿Hacía dónde vamos?</a></li>
+								<li><a href="#">¿Quiénes Participan?</a></li>
+								<li><a href="#">Planes a futuro</a></li>
+								<li><a href="#">Ventajas de ser parte de la comunidad</a></li>
+								<li><a href="#">Algunas características de las residencias</a></li>
+								<li><a href="#">Diseño y homologación de las propiedades y sus servicios</a></li>
+								<li><a href="#">Destinos</a></li>
+								<li><a href="#">Conclusión</a></li>
+							</ul>
+							<div class="download-info"><a href="#">Descargar información <i class="icon-download"></i></a></div>
+						</div>
+						<div class="col-md-9">
+							<p>Jorge y Bibi, somos los iniciadores de este novedoso concepto, que pretende brindar y superar; con los más altos estándares de Calidad, Precio, Servicio, Comodidad, Ubicación, etc.; un servicio de hospedaje con fines recreativos, de convivencia familiar o de trabajo, a todos los usuarios de nuestra red denominada IL ENCUENTRO.</p>
+
+							<p>Iniciamos operaciones en Marzo de 2013, con la puesta en marcha de la Residencia de San Antonio Tex y a partir de Julio del mismo año, incluimos la residencia de Acapulco Gro. Próximamente, a partir de Agosto de 2015, contaremos con una residencia en Cancún.</p>
+
+							<p>IL ENCUENTRO surge de la observación personal y de la opinión de nuestros amigos respecto a lo complicado que resulta muchas veces el planear unas vacaciones en familia o encontrar una propiedad en 
+							algún destino turístico o de trabajo, que se adapte a nuestras necesidades económicas, gustos y ubicación.</p>
+
+							<p>Es así como IL ENCUENTRO nace tratando de resolver esta complejidad de encontrar el lugar optimo de 
+									hospedaje vacacional o de trabajo, con características que nos diferencian del resto de la competencia:</p>
+							<p>
+								<span class="capital-letter">A) </span>
+								Exclusividad: el servicio es exclusivo a una red social de personas que sean nuestros amigos, familia, colegas, proveedores y clientes. De esta manera el uso de las residencias es de una manera controlada y con el conocimiento de por quienes están siendo utilizadas en todo momento.
+							</p>
+							<p>
+								<span class="capital-letter">B)</span> 
+								Ubicación: se buscan condiciones de comodidad, precio, ubicación y estilo para cada una de nuestras residencias. Al día de hoy tenemos 2 residencias ubicadas en Acapulco, Gro y San Antonio, Texas que cumplen con las condiciones que pretendemos brindar en nuestro servicio.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--.projectdetails-content-->
+		<!--footer-->
+		<footer>
+			<div class="container">
+				<div class="travels-copyright">
+					<a href="#">Aviso de privacidad</a>
+				</div>
+			 
+			</div>
+		</footer>
+		<!--/footer-->
+
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<script src="js/ie10-viewport-bug-workaround.js"></script>
+		<!-- jQuery -->
+		<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.min.js">\x3C/script>')</script>
+
+		  <!-- FlexSlider -->
+		  <script defer src="js/jquery.flexslider.js"></script>
+
+		  <script type="text/javascript">
+			
+			$(window).load(function(){
+			  $('.flexslider').flexslider({
+				animation: "slide",
+				start: function(slider){
+				  $('body').removeClass('loading');
+				}
+			  });
+			});
+		  </script>
+	</body>
+</html>
